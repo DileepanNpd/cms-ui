54b6fb17ffdc409686dab5a94bd7c233753b58c9
Dileepan <npdsoftwares@gmail.com>
2021-04-11 17:56:18 +0530

    ads

--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
-import { Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChanges, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ViewStory, Constants, CommonResponse } from '../model/common';
 import { environment } from 'src/environments/environment';
@@ -15,7 +15,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./story.component.css'],
   encapsulation: ViewEncapsulation.None,
 })
-export class StoryComponent implements OnInit, OnChanges, AfterViewInit {
+export class StoryComponent implements OnInit, OnChanges {
   show: boolean = false;
   storyDescription: string = '';
   viewStory!: ViewStory;
@@ -214,6 +214,8 @@ export class StoryComponent implements OnInit, OnChanges, AfterViewInit {
   ngAfterViewInit() {
     setTimeout(() => {
       try {
+        (window['adsbygoogle'] = window['adsbygoogle'] || []).push({});
+        (window['adsbygoogle'] = window['adsbygoogle'] || []).push({});
         (window['adsbygoogle'] = window['adsbygoogle'] || []).push({});
         (window['adsbygoogle'] = window['adsbygoogle'] || []).push({});
         (window['adsbygoogle'] = window['adsbygoogle'] || []).push({});
